415c475d88424bb8c901fe3a94444b33ac62fb17
Raymond Loman <raymond@carpago.nl>
2019-03-20 14:34:57 +0100

    Add spin related stuff

--- src/nl/youngcapital/kermis/attracties/Attractie.java
+++ src/nl/youngcapital/kermis/attracties/Attractie.java
@@ -3,12 +3,17 @@ package nl.youngcapital.kermis.attracties;
 public class Attractie {
 
    public String attractieNaam;
-   public int prijs;
+   public double prijs;
    public int oppervlakte;
 
    public Attractie() {
    }
    
+   public Attractie(String attractieNaam, double price) {
+     this.attractieNaam = attractieNaam;
+     this.prijs = price;
+   }
+
    public void attractieDraaien() {
       System.out.println("De attractie " + attractieNaam + " draait!");
    }
